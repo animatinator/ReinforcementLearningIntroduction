638f7e7fa97ddc73b9e2a5012282cab482d7f79d
animatinator <animatinator@gmail.com>
2020-03-30 23:09:40 +0100

    Add the possibility to run the playground with a random policy
    The control method is now configurable with the --control flag.
    
    The 'human' control method lets the user control the agent directly, as before.
    
    The 'policy' control method creates a random policy and runs it continually on
    the agent until the user exits the playground.

--- 5-8/track_playground.py
+++ 5-8/track_playground.py
@@ -1,5 +1,7 @@
 # A PyGame tool for testing out the track environment.
 
+import argparse
+from policy import Policy
 import pygame
 from track import Action, InspectableTrackEnvironent, read_track
 
@@ -9,6 +11,58 @@ TILE_SIZE = 6
 GRASS_COLOUR = (100, 200, 100)
 ROAD_COLOUR = (50, 50, 50)
 CAR_COLOUR = (200, 200, 0)
+
+
+class HumanController:
+	def __init__(self, env):
+		self._env = env
+		self._total_reward = 0
+		
+	def step(self, events):
+		for event in events:
+			if event.type == pygame.KEYDOWN:
+				if event.key not in {pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_SPACE}:
+					continue
+				
+				action = Action(0, 0)
+				if event.key == pygame.K_LEFT:
+					action = Action(-1, 0)
+				if event.key == pygame.K_RIGHT:
+					action = Action(1, 0)
+				if event.key == pygame.K_UP:
+					action = Action(0, 1)
+				if event.key == pygame.K_DOWN:
+					action = Action(0, -1)
+				
+				timestep = env.step(action)
+				self._total_reward += timestep.reward
+				print(timestep)
+				
+				if (timestep.terminal):
+					print("Goal reached! Total reward: {}".format(self._total_reward))
+					print("Resetting")
+					env.reset()
+					self._total_reward = 0
+
+
+class PolicyController:
+	def __init__(self, env, policy):
+		self._env = env
+		self._policy = policy
+		self._total_reward = 0
+	
+	def step(self, events):
+		state = env.get_state()
+		action = self._policy.get_action(state)
+		timestep = env.step(action)
+		self._total_reward += timestep.reward
+		print(timestep)
+		
+		if (timestep.terminal):
+			print("Goal reached! Total reward: {}".format(self._total_reward))
+			print("Resetting")
+			env.reset()
+			self._total_reward = 0
 
 
 def _draw_env(env, screen):
@@ -25,43 +79,21 @@ def _draw_env(env, screen):
 	pygame.draw.rect(screen, CAR_COLOUR, (pos[0] * TILE_SIZE, pos[1] * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
 
-def game_loop(env):
+def game_loop(env, controller):
 	pygame.display.set_caption("Track environment playground")
 	size = env.size()
 	screen = pygame.display.set_mode([size[0] * TILE_SIZE, size[1] * TILE_SIZE])
 	
 	clock = pygame.time.Clock()
-	
-	total_reward = 0
 
 	running = True
 	while running:
-		for event in pygame.event.get():
+		events = pygame.event.get()
+		for event in events:
 			if event.type == pygame.QUIT:
 				running = False
-			if event.type == pygame.KEYDOWN:
-				if event.key not in {pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN, pygame.K_SPACE}:
-					continue
-				
-				action = Action(0, 0)
-				if event.key == pygame.K_LEFT:
-					action = Action(-1, 0)
-				if event.key == pygame.K_RIGHT:
-					action = Action(1, 0)
-				if event.key == pygame.K_UP:
-					action = Action(0, 1)
-				if event.key == pygame.K_DOWN:
-					action = Action(0, -1)
-				
-				timestep = env.step(action)
-				total_reward += timestep.reward
-				print(timestep)
-				
-				if (timestep.terminal):
-					print("Goal reached! Total reward: {}".format(total_reward))
-					print("Resetting")
-					env.reset()
-					total_reward = 0
+		
+		controller.step(events)
 		
 		_draw_env(env, screen)
 		pygame.display.flip()
@@ -70,11 +102,23 @@ def game_loop(env):
 
 
 if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--control')
+	args = parser.parse_args()
+	
 	pygame.init()
 
-	track = read_track("track.bmp")
+	track = read_track('track.bmp')
 	env = InspectableTrackEnvironent(track)
 	
-	game_loop(env)
+	if args.control == 'human':
+		controller = HumanController(env)
+	elif args.control == 'policy':
+		policy = Policy(env.size(), 6)
+		controller = PolicyController(env, policy)
+	else:
+		raise ValueError('Invalid control type: {}. Try \'human\' or \'policy\'.'.format(args.control))
+	
+	game_loop(env, controller)
 	
 	pygame.quit()
