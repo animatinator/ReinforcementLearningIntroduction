fed8aead0bf8e19e16e60257d991d19e0566b8b8
animatinator <animatinator@gmail.com>
2020-04-04 14:04:19 +0100

    Implement most of the off-policy MC algorithm
    This is missing a computation of the likelihood of picking a given action with
    the epsilon-greedy policy, which will be slightly more fiddly. Just committing
    now before I get into that.

--- 5-8/train.py
+++ 5-8/train.py
@@ -37,3 +37,30 @@ if __name__ == '__main__':
 			print("Training step {}...".format(i))
 		soft_policy = EpsilonGreedyPolicy(EPSILON, Pi, NUM_ACTIONS)
 		episode = rollout(env, soft_policy)
+		
+		T = len(episode) - 1
+		
+		G = 0
+		W = 1
+		
+		for t in range(T-1, -1, -1):
+			# Get key variables from this episode step.
+			St = episode[t].state
+			At = episode[t].action
+			Rt_1 = episode[t+1].reward
+
+			# Update Q-values and visit counts.
+			G = (LAMBDA * G) + Rt_1
+			Q.increment_count(St, At, W)
+			Qs_a = Q.get(St, At)
+			new_Qs_a = Qs_a + (W / Q.get_count(St, At)) * (G - Qs_a)
+			Q.set(St, At, new_Qs_a)
+			
+			# Update the policy.
+			Pi.update(St, Q.get_max_action(St))
+			
+			# Stop this episode if it's no longer behaving greedily.
+			if At != Pi.get_action(St):
+				break
+			
+			W += 1 / (b_a_S)
