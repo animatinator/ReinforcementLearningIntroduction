f7356b6d014eba670d4859bed5f34886dc716c74
animatinator <animatinator@gmail.com>
2020-04-18 12:36:45 +0100

    Add a basic blackjack environment

---
+++ blackjack/environment.py
@@ -0,0 +1,87 @@
+# Blackjack environment
+from numpy import random
+
+# 1-9 and four tens
+BLACKJACK_CARDS = list(range(1, 10)) + [10]*4
+
+class TimeStep:
+	def __init__(self, observation, reward, terminal):
+		self.observation = observation
+		self.reward = reward
+		self.terminal = terminal
+	
+	def __str__(self):
+		return "{}: {}, reward: {}".format("Terminal state" if self.terminal else "State", self.observation, self.reward)
+
+class State:
+	def __init__(self, initial_hand):
+		self._hand = []
+		
+	def deal(self, card):
+		self._hand.append(card)
+		
+	def usable_ace(self):
+		return 1 in self._hand and sum(self._hand) + 10 <= 21
+		
+	def sum(self):
+		if self.usable_ace():
+			return sum(self._hand) + 10
+		return sum(self._hand)
+		
+	def bust(self):
+		return self.sum() > 21
+
+class Blackjack:
+	def __init__(self):
+		self._reset()
+	
+	def _make_observation(self):
+		return (self._player.sum(), self._dealer.sum(), self._player.usable_ace())
+		
+	def _reset(self):
+		player_cards = self._draw_hand()
+		dealer_cards = self._draw_hand()
+		
+		while sum(player_cards) < 12:
+			player_cards.append(self._draw())
+		
+		self._player = State(player_cards)
+		self._dealer = State(dealer_cards)
+		
+	def reset(self):
+		self._reset()
+		
+		return self._make_observation()
+		
+	def _draw_hand(self):
+		return [self._draw(), self._draw()]
+	
+	def _draw(self):
+		return random.choice(BLACKJACK_CARDS)
+		
+	def _compute_score(self, player_score, dealer_score):
+		if player_score == dealer_score:
+			return 0
+		elif player_score > dealer_score:
+			return 1
+		else:
+			return -1
+		
+	def step(self, stick):
+		done = False
+
+		if stick:
+			self._player.deal(self._draw())
+			
+			if self._player.bust():
+				reward = -1
+				done = True
+			else:
+				reward = 0
+		else:
+			done = True
+			while self._dealer.sum() < 17:
+				self._dealer.deal(self._draw())
+			reward = self._compute_score(self._player.sum(), self._dealer.sum())
+		
+		return TimeStep(self._make_observation(), reward, done)
---
+++ blackjack/playground.py
@@ -0,0 +1,27 @@
+import environment
+
+def print_observation(observation):
+    score, dealer_score, usable_ace = observation
+    print("Player Score: {} (Usable Ace: {}), Dealer Score: {}".format(
+          score, usable_ace, dealer_score))
+
+def strategy(observation):
+    score, dealer_score, usable_ace = observation
+    # Stick (action 0) if the score is > 20, hit (action 1) otherwise
+    return 0 if score >= 20 else 1
+
+	
+if __name__ == '__main__':
+	env = environment.Blackjack()
+	for i_episode in range(20):
+		observation = env.reset()
+		for t in range(100):
+			print_observation(observation)
+			action = strategy(observation)
+			print("Taking action: {}".format( ["Stick", "Hit"][action]))
+			timestep = env.step(action)
+			observation = timestep.observation
+			if timestep.terminal:
+				print_observation(observation)
+				print("Game end. Reward: {}\n".format(float(timestep.reward)))
+				break
