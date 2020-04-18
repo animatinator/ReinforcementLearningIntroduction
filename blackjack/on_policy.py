16562da40403f183f1a36be53e70b1dbb2ab9a58
animatinator <animatinator@gmail.com>
2020-04-18 14:13:23 +0100

    Implement basic algorithm

--- blackjack/on_policy.py
+++ blackjack/on_policy.py
@@ -5,6 +5,11 @@ import numpy as np
 import policy
 
 
+EPSILON = 0.2
+TRAIN_STEPS = 1000
+LAMBDA = 0.9
+
+
 def rollout(policy):
 	episode = []
 
@@ -21,7 +26,45 @@ def rollout(policy):
 	return episode
 
 
+def process_for_first_visits(rollout):
+	seen = set()
+	result = []
+
+	for step in rollout:
+		if (step[0], step[1]) in seen:
+			result.append(step + [False])
+		else:
+			seen.add((step[0], step[1]))
+			result.append(step + (True,))
+
+	return result
+
+
 if __name__ == '__main__':
-	policy = policy.Policy()
-	policy.e_soften(0.5)
-	print(rollout(policy))
+	pi = policy.Policy()
+	pi.e_soften(EPSILON)
+	q = policy.QValues()
+	returns = policy.ReturnCounter()
+
+	for i in range(TRAIN_STEPS):
+		episode = process_for_first_visits(rollout(pi))
+		G = 0
+
+		for i in range(len(episode) - 1, -1, -1):
+			step = episode[i]
+			G = (LAMBDA * G) + step[2]
+
+			if step[3]:  # Indicates whether this is the first visit.
+				observation = step[0]
+				player_score = observation[0]
+				dealer_score = observation[1]
+				usable_ace = observation[2]
+				action = step[1]
+				returns.add_return(player_score, dealer_score, usable_ace, action, G)
+				q.update(player_score, dealer_score, usable_ace, action,
+					returns.average_return(player_score, dealer_score, usable_ace, action))
+				best_action = q.pick_best(player_score, dealer_score, usable_ace)
+
+				policy_update = policy.e_soften(
+					np.array([0.0, 1.0]) if best_action else np.array([1.0, 0.0]), EPSILON)
+				pi.set(player_score, dealer_score, usable_ace, policy_update)
--- blackjack/policy.py
+++ blackjack/policy.py
@@ -4,6 +4,10 @@ import numpy as np
 from numpy import random
 
 
+def bool_to_int(bool_val):
+	return 1 if bool_val else 0
+
+
 def e_soften(array, e):
 	softened = array * (1.0 - e) + (e / len(array))
 	# Normalise before returning.
@@ -23,14 +27,23 @@ class ReturnCounter:
 
 class QValues:
 	def __init__(self):
-		# (player_score, dealer_score, usable_ace)
-		self._q = random.rand(22, 11, 2)
+		# (player_score, dealer_score, usable_ace, stick/hit)
+		self._q = random.rand(22, 11, 2, 2)
 		
-	def get(self, player_score, dealer_score, usable_ace):
-		return self._q[player_score][dealer_score][usable_ace]
+	def get(self, player_score, dealer_score, usable_ace, action):
+		action = bool_to_int(action)
+		usable_ace = bool_to_int(usable_ace)
+		return self._q[player_score][dealer_score][usable_ace][action]
 	
-	def update(self, player_score, dealer_score, usable_ace, new_value):
-		self._q[player_score][dealer_score][usable_ace] = new_value
+	def update(self, player_score, dealer_score, usable_ace, action, new_value):
+		action = bool_to_int(action)
+		usable_ace = bool_to_int(usable_ace)
+		self._q[player_score][dealer_score][usable_ace][action] = new_value
+
+	def pick_best(self, player_score, dealer_score, usable_ace):
+		usable_ace = bool_to_int(usable_ace)
+		possibilities = self._q[player_score][dealer_score][usable_ace]
+		return np.argmax(possibilities)
 
 
 class Policy:
@@ -44,6 +57,10 @@ class Policy:
 			for d in range(11):
 				for u in range(2):
 					self._pi[p][d][u] = e_soften(self._pi[p][d][u], epsilon)
+
+	def set(self, player_score, dealer_score, usable_ace, values):
+		usable_ace = bool_to_int(usable_ace)
+		self._pi[player_score][dealer_score][usable_ace] = values
 	
 	def act(self, player_score, dealer_score, usable_ace):
 		action_probabilities = self._pi[player_score][dealer_score][1 if usable_ace else 0]
