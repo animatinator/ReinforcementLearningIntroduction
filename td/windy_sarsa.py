53caa143bf4dee6180e2f079ed745cdddf25efb5
animatinator <animatinator@gmail.com>
2020-05-02 14:27:05 +0100

    Add Q-value representation and epsilon-greedy selection

--- td/windy_sarsa.py
+++ td/windy_sarsa.py
@@ -1,6 +1,41 @@
 # Windy gridworld SARSA solution
 
+import numpy as np
+import random
 from windy_env import Action, TimeStep, WindyGridworld
+
+
+EPSILON = 0.1
+
+
+class QFunction:
+	def __init__(self, width, height):
+		self._q = np.zeros((height, width, len(Action)), dtype=np.float32)
+
+	def get_value(self, state, action):
+		return self._q[state.y][state.x][action.value]
+
+	def set_value(elf, state, action, value):
+		self._q[state.y][state.x][action.value] = value
+
+	def optimal_action(self, state, possible_actions):
+		# Absolute mess. Trying to get the maximum action from the possible actions.
+		# First get the Q-values, and bind them with the actions they represent, ie.
+		# (Q-value, Action).
+		action_values = self._q[state[1]][state[0]]
+		linked_to_actions = np.array([(val, Action(i)) for i, val in enumerate(action_values)])
+		# Filter down to the actions that are possible.
+		possibility_filter = np.isin(linked_to_actions[:, 1], list(possible_actions))
+		possible_options = linked_to_actions[possibility_filter]
+		# Return the action corresponding to the maximum Q-value.
+		return possible_options[np.argmax(possible_options[:, 0])][1]
+
+
+def e_greedy_action(state, possible_actions, q_function, epsilon):
+	if (np.random.uniform() < epsilon):
+		return random.sample(possible_actions)
+	else:
+		return q_function.optimal_action(state, possible_actions)
 
 
 if __name__ == '__main__':
@@ -9,3 +44,4 @@ if __name__ == '__main__':
 	goal_pos = (7, 3)
 
 	env = WindyGridworld(winds, height, goal_pos)
+	q = QFunction(width=len(winds), height=height)
