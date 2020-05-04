4e92ef7676a3860c0ab2bdc020f92f18129aba80
animatinator <animatinator@gmail.com>
2020-05-05 00:18:15 +0100

    Implement basic n-step TD

--- td/simple_td.py
+++ td/simple_td.py
@@ -6,6 +6,8 @@
 
 from dataclasses import dataclass
 from enum import Enum
+import math
+from matplotlib import pyplot
 import random
 
 
@@ -14,8 +16,10 @@ STATES = 5
 # The state to start in (zero-based index, including left terminal).
 START_STATE = 3
 
-ALPHA = 0.1
-TRAIN_STEPS = 100
+ALPHA = 0.05
+TRAIN_STEPS = 10000
+N_STEP_ALPHA = 0.15
+N = 3
 
 
 @dataclass
@@ -58,10 +62,18 @@ def update_value(state, values, value):
 		values[state-1] = value
 
 
+def rms_error(values):
+	sum = 0.0
+	for i in range(len(values)):
+		expected_value = float(i + 1) / float(len(values) + 1)
+		sum += (values[i] - expected_value) ** 2
+	return math.sqrt(sum)
+
+
 def run_td(values):
 	state = START_STATE
 	step = TimeStep(START_STATE, 0, False)
-	
+
 	while not step.terminal:
 		action = Action(random.randint(0, len(Action) - 1))
 		step = make_step(state, action)
@@ -70,14 +82,66 @@ def run_td(values):
 		next_state_val = get_value(step.state, values)
 		new_val = old_val + ALPHA * ((step.reward + next_state_val) - old_val)
 		update_value(state, values, new_val)
-		
+
 		state = step.state
+
+
+def n_step_update(past_results, values):
+	state_to_update = past_results[0][0]
+	latest_state = past_results[-1][0]
+	past_results.pop(0)
+	latest_estimate = get_value(latest_state, values)
+	reward_sum = sum([entry[1] for entry in past_results]) + latest_estimate
+	old_val = get_value(state_to_update, values)
+	new_val = old_val + ALPHA * (reward_sum - old_val)
+	update_value(state_to_update, values, new_val)
+
+
+def run_n_step_td(values, n):
+	state = START_STATE
+	step = TimeStep(START_STATE, 0, False)
+
+	past_results = [(START_STATE, 0)]
+
+	while not step.terminal:
+		action = Action(random.randint(0, len(Action) - 1))
+		step = make_step(state, action)
+		past_results.append((step.state, step.reward))
+
+		if len(past_results) > n + 1:
+			n_step_update(past_results, values)
+
+		state = step.state
+
+	while past_results:
+		n_step_update(past_results, values)
+
+
+def plot_errors(errors):
+	xs = [i for i in range(len(errors))]
+	pyplot.scatter(xs, errors)
+	pyplot.show()
 
 
 if __name__ == '__main__':
 	values = [0.5] * 5
+	errors = []
 
 	for i in range(0, TRAIN_STEPS):
 		run_td(values)
-	
-	print(values)
+		errors.append(rms_error(values))
+
+	print("Simple TD result: {}".format(values))
+	print("Drawing error graph...")
+	plot_errors(errors)
+
+	n_step_values = [0.5] * 5
+	errors = []
+
+	for i in range(0, TRAIN_STEPS):
+		run_n_step_td(n_step_values, N)
+		errors.append(rms_error(n_step_values))
+
+	print("{}-step TD result: {}".format(N, n_step_values))
+	print("Drawing error graph...")
+	plot_errors(errors)
